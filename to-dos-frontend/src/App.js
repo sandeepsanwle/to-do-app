96bbd82803b99d237b363d946e059807cd7435b6
sandeep sanwle <sandeepsanwle74@gmail.com>
2023-11-01 22:39:42 +0530

    basic routing setup

--- to-dos-frontend/src/App.js
+++ to-dos-frontend/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Login from './components/login/login';
+import Signup from './components/signup/signup';
+import Dashboard from './components/dashboard/dashboard';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NopageFound from './components/nopagefound/NopageFound';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello World!</h2>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<NopageFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
