daa066c52adcc9afe9f95ac33ac76d4f46336196
sandeep sanwle <sandeepsanwle74@gmail.com>
2023-11-02 23:04:31 +0530

    to dos dashboard page design

--- to-dos-frontend/src/components/dashboard/dashboard.jsx
+++ to-dos-frontend/src/components/dashboard/dashboard.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import "./dashboard.css";
 
 function Dashboard() {
+  const currentDate = new Date();
+ 
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
+  const formattedDate = currentDate.toLocaleDateString('en-US', options);
+
+
   return (
-    <div>dashboard</div>
-  )
+    <div className="dashboardpage">
+      <div class="context">
+        <div className="date">
+        <h3>{formattedDate}</h3>
+        </div>
+        <br />
+        <div className="todo">
+        </div>
+      </div>
+      <div class="area">
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
